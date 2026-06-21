b65eb971bf878ec7ea4f11a87add2ddc08a562b7
NaomiSoubhia <soubhiadoinaomi@gmail.com>
2026-06-21 15:57:29 -0400

    Add Joi validation schema for book creation

--- index.js
+++ index.js
@@ -32,7 +32,13 @@ app.get('/api/books/:id', (req, res) =>{
 
 //POST
 app.post('/api/books', (req,res) =>{
-    if(!req.body.nam || req.body.name.length < 3){
+    const schema = {
+        name: Joi.string().min(3).required()
+    };
+
+    const result = Joi.validate(req.body,schema);
+    console.log(result);
+    if(!req.body.name || req.body.name.length < 3){
         //400 Bad Request
         res.status(400).send('Name is required and should be minimum 3 character.');
         return;
