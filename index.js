d392c513f32e764e99a946f11af5792251d795ea
NaomiSoubhia <soubhiadoinaomi@gmail.com>
2026-06-21 13:32:04 -0400

    Configure server port using environment variable

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 
 app.get('/', (req, res)=> {
 
-    res.send('Hello World');
+    res.send('Hello World!!!');
 
 });
 
@@ -11,4 +11,9 @@ app.get('/api/book', (req,res) =>{
 res.send([1,2,3]);
 });
 
-app.listen(3000, ()=> console.log('Listening in the port 3000'));
+//PORT enviroment variable 
+//process global object that has properties
+//if we dont have a global variable called port, it will use port 3000
+const port = process.env.PORT || 3000;
+
+app.listen(port, ()=> console.log(`Listening in the port ${port}`));
